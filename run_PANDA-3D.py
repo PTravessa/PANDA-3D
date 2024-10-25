244309570f19ef10de3a3a483595589ffb863505
PTravessa <pedrotravessa00@gmail.com>
2024-10-25 16:13:00 +0100

    Working panda3dcode

--- run_PANDA-3D.py
+++ run_PANDA-3D.py
@@ -1,10 +1,10 @@
-import sys;
+import sys
 tag_ = 'panda3dcode/'
 sys.path.append(tag_)
 
 import numpy as np
 import pandas as pd
-import sys,os
+import sys, os
 import datetime
 import pickle
 import copy
@@ -22,9 +22,8 @@ import json
 import time
 from argparse import Namespace
 
-
-from gvp_transformer import GVPTransformerModel # modified Model
-from data import Alphabet, Alphabet_goclean  # modified token for GO terms
+from gvp_transformer import GVPTransformerModel
+from data import Alphabet, Alphabet_goclean
 import esm
 from model_util import get_n_params, CreateDataset_Server, BatchGvpesmConverter
 from torch.utils.data import DataLoader
@@ -34,12 +33,12 @@ def predicate(model, device, dataloader, prediction_go_mask):
     loss_batch = []
     protein, prediction = [], []
     with torch.no_grad():
-        for i,data in enumerate(dataloader):
-            model_out = model(data['esm1vs'],data['coords'], data['seqs'], data['padding_mask'], data['plddts'],
+        for i, data in enumerate(dataloader):
+            model_out = model(data['esm1vs'], data['coords'], data['seqs'], data['padding_mask'], data['plddts'],
                         return_all_hiddens=False)
-            logits = torch.squeeze(model_out[0],1)
+            logits = torch.squeeze(model_out[0], 1)
             protein.append(data['proteins'][0])
-            prediction_ = torch.sigmoid(logits).cpu().detach().numpy()[:,prediction_go_mask]
+            prediction_ = torch.sigmoid(logits).cpu().detach().numpy()[:, prediction_go_mask]
             prediction.append(prediction_[0])
     test_df = pd.DataFrame({'protein': protein,
                             'prediction': prediction})
@@ -51,30 +50,27 @@ def prediction2text(alphabet, predictions, prediction_format_f):
     for i, row in enumerate(predictions.itertuples()):
         target = row.protein
         scores = row.prediction
-        # sort scores
         scores_dic = {}
-        for i,score in enumerate(scores):
-            score = round(score,2)
-            if score > 1 or score<= 0.09:
+        for i, score in enumerate(scores):
+            score = round(score, 2)
+            if score > 1 or score <= 0.09:
                 continue
             token = alphabet.all_toks[i]
             if 'GO' in token:
                 scores_dic[token] = score
-        scores_sorted = dict(sorted(scores_dic.items(), key=lambda item: item[1],reverse=True))
-        # save scores
+        scores_sorted = dict(sorted(scores_dic.items(), key=lambda item: item[1], reverse=True))
         for key in scores_sorted:
             prediction_format_fh.write(f'{target}\t{key}\t{"%.2f" % scores_sorted[key]}\n')    
     prediction_format_fh.write('END\n')
     prediction_format_fh.close()
 
 if True:
-    # python train.py
     start = time.time()
     args_f = f'{tag_}/args.json'
-    model_args = json.load(open(args_f,'r'))
+    model_args = json.load(open(args_f, 'r'))
     model_args = Namespace(**model_args)
-    # use cpu for testing
-    model_args.device = ['cuda:0']
+    # Use CPU instead of GPU
+    model_args.device = ['cpu']
 
     alphabet = Alphabet.from_architecture(model_args.arch)
     terms = list(pd.read_pickle(tag_ + model_args.terms_pkl))
@@ -85,7 +81,6 @@ if True:
     out_dir = f'{input_dir}'
     out_file = f'{out_dir}/prediction.txt'
     
-    # all tokens: GO-> True; others-> False
     prediction_go_mask = np.array(['GO' in tok for tok in alphabet_go.all_toks])
     
     model = GVPTransformerModel(
@@ -93,12 +88,11 @@ if True:
                 alphabet,
                 alphabet_go,
         )
-    model.load_state_dict(torch.load(out_model,map_location='cuda:0'))
+    model.load_state_dict(torch.load(out_model, map_location=torch.device('cpu')))
     model.to(model_args.device[0])
     
     test_data = CreateDataset_Server(input_dir, batch_size=1)
     batch_gvpesm_converter = BatchGvpesmConverter(alphabet, alphabet_go, model_args.coords_mask_plddt_th, model_args.device[0])
-    test_dataloader = DataLoader(test_data, batch_size= 1, shuffle=True, collate_fn=batch_gvpesm_converter)
+    test_dataloader = DataLoader(test_data, batch_size=1, shuffle=True, collate_fn=batch_gvpesm_converter)
     test_df = predicate(model, model_args.device[0], test_dataloader, prediction_go_mask)
-    prediction2text(alphabet_go, test_df,  out_file)
-    #print(f'done, save to {out_file}')
+    prediction2text(alphabet_go, test_df, out_file)
